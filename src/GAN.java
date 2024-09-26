0c61aacabd9c88e5c14e427801eae34c22b97199
Quinterr <vank2407@gmail.com>
2024-09-26 17:19:30 +0300

    Suiii

--- src/GAN.java
+++ src/GAN.java
@@ -6,15 +6,30 @@ public class GAN {
         Random rn = new Random();
         Scanner scanner = new Scanner(System.in);
         boolean toggle;
-        int a, b, i;
+        int a, b, i, z, x, w;
 
         System.out.println("Вы хотите начать игру? д/н");
         if (scanner.next().equalsIgnoreCase("д")) {
-            toggle = true;
+            System.out.println("Выберите режим игры: \n 1 - числа от 0 до 100, 7 попыток\n 2 - числа от 0 до 1000, 10 попыток\n");
+            while (true) {
+                w = scanner.nextInt();
+                if ((w > 2) || (w < 1)){
+                    System.out.println("Выберете режим 1 или 2");
+                } else {
+                    break;
+                }
+            }
 
+            if (w == 1) {
+                z = 101; x = 7;
+            } else {
+                z = 1001; x = 10;
+            }
+
+            toggle = true;
             while (toggle) {
-                a = rn.nextInt(0,101);
-                i = 7;
+                a = rn.nextInt(0, z);
+                i = x;
                 System.out.println("Я загадал число от 0 до 100. Попробуй его отгадать");
 
                 while (i != 0) {
